af7ecc5110f5610438dbabb47a5f0af979f7e92e
ortv <ortv617@gmail.com>
2023-03-19 13:49:32 +0200

    almost finish stage 1-final tests

--- src/primitives/Point.java
+++ src/primitives/Point.java
@@ -1,36 +1,62 @@
 package primitives;
 import java.lang.Math;
 import java.util.Objects;
+/**
+ * a Point class
+ * @author AAA
+ *
+ */
 public class Point {
+	/**
+	 * ctor for Point class how gets 3 double numbers
+	 * @param x num1	
+	 * @param y num2
+	 * @param z num3
+	 */
  public Point(double x, double y, double z)
  {
 	 xyz= new Double3(x,y,z);
 }
+ /**
+  * ctor for Point class that gets a number of Double3-that consists of 3 double numbers
+  * @param x
+  */
  public Point(Double3 x)
  {
 	 xyz= new Double3(x.d1,x.d2,x.d3);
  }
- /*
-  * gets a point and  subtract   from the current point by sending to  an existing Double3 function-return a vector
+/**
+ * subtract between two points
+ * @param p is the given point
+ * @return a new vector-point which is the result of the subtraction
  */
  public Vector subtract(Point p)
  {
 	return new Vector(xyz.subtract(p.xyz));	
  }
+ /**
+  * add between two points(vector is kind of point)
+  * @param v is the given vector
+  * @return a new point which is the result of the subtraction
+  */
  public Point add(Vector v)
  {
 	 return new Point(xyz.add(v.xyz));
  }
- /*
-  * return the distance between two points in a pow
- */
+ /**
+  * calculate the distance between two points-without squrt
+  * @param p the given point to calculate the distance from
+  * @return distance in a pow
+  */
  public double distanceSquared(Point p)
  {
 	 return ((p.xyz.d1-xyz.d1)*(p.xyz.d1-xyz.d1)+(p.xyz.d2-xyz.d2)*(p.xyz.d2-xyz.d2)+(p.xyz.d3-xyz.d3)*(p.xyz.d3-xyz.d3));
  }
- /*
-  * return the actual distance between two points
-  * */
+ /**
+  * calculate the distance between two points
+  * @param p given point to calculate the distance from
+  * @return the actual distance between two points
+  */
  public double distance(Point p)
  {
 	 return (Math.sqrt(distanceSquared(p)));
@@ -45,8 +71,11 @@ public int hashCode() {
 	return Objects.hash(xyz);
 }
 @Override
-public boolean equals(Object obj) {
-	return xyz.equals(obj);
+public boolean equals(Object obj) {	
+	 if (this == obj) return true;
+     if (obj instanceof Point other)
+       return this.xyz.equals(other.xyz);
+     return false;
 }
 Double3 xyz;
  
--- src/primitives/Vector.java
+++ src/primitives/Vector.java
@@ -1,43 +1,86 @@
 package primitives;
+import static primitives.Util.isZero;
+
 import java.lang.Math;
 
+import javax.print.attribute.standard.MediaSize.Other;
+/**
+ * Vector class that inherit from Point
+ * @author AAA
+ *
+ */
 public class Vector extends Point
 {
-
+	/**
+	 * ctor for Vector class that gets 3 double numbers
+	 * @param x num1
+	 * @param y num2
+	 * @param z num3
+	 */
 	public Vector(double x, double y, double z) 
 	{
 		super(x, y, z);
 		// TODO Auto-generated constructor stub
-		/*k855#$%^&*()_)O(*&^%????????????*/
-		if(new Double3(x,y,z).equals(Double3.ZERO))
+		if(xyz.equals(Double3.ZERO))
 			throw new IllegalArgumentException("cant use a zero vector!");
 	}
+	/**
+	 * ctor for Vector class that gets number of Double3 that consists of 3 double numbers
+	 * @param p
+	 */
 	public Vector(Double3 p) 
 	{
 		super(p);// TODO Auto-generated constructor stub
 		if(p.equals(Double3.ZERO))
 			throw new IllegalArgumentException("cant use a zero vector!");
 	}
+	/**
+	 * add between 2 vectors and return the new vector of the result
+	 */
 	public Vector add(Vector v)
 	{
 		return new Vector(this.xyz.add(v.xyz));
 	}
+	/**
+	 * scale vector and number
+	 * @param num
+	 * @return new vector of the result
+	 */
 	public Vector scale(double num)
 	{
 		return new Vector(this.xyz.scale(num));
 	}
+	/**
+	 * cross product between  two vector
+	 * @param v the given vector
+	 * @return new vector of the result
+	 */
 	public Vector crossProduct(Vector v)
 	{
-		return new Vector(this.xyz.d2*v.xyz.d3-this.xyz.d3*v.xyz.d2,this.xyz.d3*v.xyz.d1-this.xyz.d1*v.xyz.d3,this.xyz.d1*v.xyz.d2-this.xyz.d2*this.xyz.d2*v.xyz.d1);
+		return new Vector(this.xyz.d2*v.xyz.d3-this.xyz.d3*v.xyz.d2,
+				this.xyz.d3*v.xyz.d1-this.xyz.d1*v.xyz.d3,
+				this.xyz.d1*v.xyz.d2-this.xyz.d2*v.xyz.d1);
 	}
+	/**
+	 * calculates length of vector without squrt
+	 * @return the length
+	 */
 	public double lengthSquared()
 	{
 		return (this.xyz.d1*this.xyz.d1)+(this.xyz.d2*this.xyz.d2)+(this.xyz.d3*this.xyz.d3);
 	}
+	/**
+	 * calculate the actual length of vector
+	 * @return length
+	 */
 	public double length()
 	{
 		return Math.sqrt(this.lengthSquared());
 	}
+	/**
+	 * calculate normal vector for the current vector
+	 * @return normal vector
+	 */
 	public Vector normalize()
 	{
 		double len=this.length();
@@ -53,8 +96,16 @@ public class Vector extends Point
 	}
 	@Override
 	public boolean equals(Object obj) {
-		return super.equals(obj);
+		      if (this == obj) return true;
+		      if (obj instanceof Vector other)
+		        return this.xyz.equals(other.xyz);
+		      return false;
 	}
+	/**
+	 * calculates dot product for vector
+	 * @param v the given vector
+	 * @return the result-double number
+	 */
 	public double dotProduct(Vector v)
 	{
 		return(this.xyz.d1*v.xyz.d1+this.xyz.d2*v.xyz.d2+this.xyz.d3*v.xyz.d3);
